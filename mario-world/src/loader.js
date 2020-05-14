e8f7fc77467ca20a11bf54126fe27fb94d9a57e1
xumufei1994 <xmfjx1994@gmail.com>
2020-05-14 22:57:48 +0800

    add models
    load several models

--- mario-world/src/loader.js
+++ mario-world/src/loader.js
@@ -3,6 +3,16 @@ import yoshiSchool from './models/yoshi4school.fbx';
 import marioMesh from './models/mario/animations/Run.fbx';
 import star from './models/Star.fbx';
 import extraLife from './models/Extra_Life.fbx';
+import boo from './models/Boo.fbx';
+import fireflower from "./models/FireFlower.fbx";
+import Goomba from './models/Goomba.fbx';
+import qmark from './models/Question_Mark_Block.fbx';
+import brickblock from './models/BrickBlock/source/Brick Block.fbx';
+import smallCastle from './models/Castle/Castle_Small.fbx';
+import mushroom from './models/mushroom.fbx';
+// import trees from './models/Cartoon_trees.fbx';
+
+
 
 function loadModels({ scene, camera }, worldObject) {
   const loader = new FBXLoader();
@@ -67,13 +77,10 @@ function loadModels({ scene, camera }, worldObject) {
     object.scale.set(1/20, 1/20, 1/20);
     object.position.set(170, 50, 25);
     scene.add(object);
-        const temp = new THREE.Vector3();
-    temp.set(object.position.x, object.position.y + 20, object.position.z - 50);
-    camera.position.lerp(temp, 1);
-    camera.lookAt(object.position);
   });
 
   loader.load(extraLife, function (object) {
+    
     object.traverse(function (child) {
 
       if (child.isMesh) {
@@ -85,6 +92,122 @@ function loadModels({ scene, camera }, worldObject) {
     object.position.set(150, 45, 25);
     scene.add(object);
   });
+
+
+  loader.load(Goomba, function (object) {
+    object.traverse(function (child) {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    object.scale.set(1 / 5, 1 / 5, 1 / 5);
+    object.position.set(90, 60, 25);
+    scene.add(object);
+  });
+  
+
+loader.load(fireflower, function (object) {
+  object.traverse(function (child) {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+
+  });
+  object.scale.set(1 / 30, 1 / 30, 1 / 30);
+  object.position.set(110, 50, 25);
+  scene.add(object);
+});
+
+//trees have a blank board, it might cannot add in our project
+// loader.load(trees, function (object) {
+//   object.traverse(function (child) {
+
+//     if (child.isMesh) {
+//       child.castShadow = true;
+//       child.receiveShadow = true;
+//     }
+//   });
+//   object.scale.set(1/3, 1/3, 1/3);
+//   object.position.set(150, 50, 25);
+//   scene.add(object);
+// });
+
+//the small ghost
+loader.load(boo, function (object) {
+  object.traverse(function (child) {
+
+    if (child.isMesh) {
+
+      // child.material.map = texture;
+      // child.material.needsUpdate = true;
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  object.scale.set(1 / 20, 1 / 20, 1 / 20);
+  object.position.set(130, 55, 25);
+  scene.add(object);
+});
+
+
+loader.load(qmark, function (object) {
+  object.traverse(function (child) {
+
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  object.scale.set(1 / 60, 1 / 60, 1 / 60);
+  object.position.set(200, 50, 25);
+  scene.add(object);
+});
+
+
+loader.load(brickblock, function (object) {
+  object.traverse(function (child) {
+
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  object.scale.set(1 / 60, 1 / 60, 1 / 60);
+  object.position.set(220, 48, 25);
+  scene.add(object);
+});
+
+loader.load(smallCastle, function (object) {
+  object.traverse(function (child) {
+
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  object.scale.set(1 / 60, 1 / 60, 1 / 60);
+  object.position.set(170, 50, 0);
+  scene.add(object);
+});
+
+
+loader.load(mushroom, function (object) {
+  object.traverse(function (child) {
+
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  object.scale.set(1 / 2, 1 / 2, 1 / 2);
+  object.position.set(240, 40, 25);
+  scene.add(object);
+});
+
+
+
 }
 
 export { loadModels }
